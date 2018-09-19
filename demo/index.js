1b18d3a38901b5b6309b07a649b1db0c40327fe1
Tim Brayshaw <tim@brayshaw.com>
2018-09-19 23:15:16 +0100

    Update demo to show using the generated gradient as a pattern fill on another context.

--- demo/index.js
+++ demo/index.js
@@ -1,13 +1,24 @@
 import { SweepGradient } from "../src/index.js";
 
-const demoElement = document.querySelector("#demo");
-const dpr = Math.ceil(window.devicePixelRatio) || 1;
-demoElement.width = demoElement.getBoundingClientRect().width * dpr;
-demoElement.height = demoElement.getBoundingClientRect().height * dpr;
-const ctx = demoElement.getContext("2d");
-ctx.scale(dpr, dpr);
+const displayElement = document.querySelector("#demo");
+const displayRect = displayElement.getBoundingClientRect();
 
-const g = new SweepGradient(ctx);
+const width = displayRect.width;
+const height = displayRect.height;
+
+/**
+ * Setup an off screen canvas context for drawing our gradient.
+ * Note: don't the scale the context when generating a graphic for use as a pattern fill.
+ */
+const offscreenElement = document.createElement('canvas');
+offscreenElement.width = width;
+offscreenElement.height = height;
+const offscreenCtx = offscreenElement.getContext("2d");
+
+/**
+ * Setup and draw the gradient.
+ */
+const g = new SweepGradient(offscreenCtx);
 g.addColorStop(0.0, "#ffff00");
 g.addColorStop(0.25, "#ff00ff");
 g.addColorStop(0.5, "red");
@@ -15,3 +26,24 @@ g.addColorStop(0.75, "rgba(255,255,0,0)");
 g.addColorStop(1.0, "#ffff00");
 
 g.draw();
+
+/**
+ * Get the gradient as a pattern.
+ */
+const gradientPattern = offscreenCtx.createPattern(offscreenCtx.canvas, "no-repeat");
+
+/**
+ * Set up the canvas on the page to place our gradient
+ */
+const dpr = Math.ceil(window.devicePixelRatio) || 1;
+displayElement.width = width * dpr;
+displayElement.height = height * dpr;
+const displayCtx = displayElement.getContext("2d");
+displayCtx.scale(dpr, dpr);
+
+/**
+ * Draw a circle, which we fill using our gradient pattern.
+ */
+displayCtx.fillStyle = gradientPattern;
+displayCtx.arc(width / 2, height / 2, width / 2, 0, 2 * Math.PI);
+displayCtx.fill();
