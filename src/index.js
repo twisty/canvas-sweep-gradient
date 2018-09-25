cb51001cc14c2dd413d0cea74fd99961ef185223
Tim Brayshaw <tim@brayshaw.com>
2018-09-25 09:38:09 +0100

    Remove tinycolor2 dependency, refactor aquisition of colours.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import tinycolor from "tinycolor2";
-
 export class SweepGradient {
   constructor(ctx, x0, y0, x1, y1) {
     this.ctx = ctx;
@@ -7,66 +5,60 @@ export class SweepGradient {
     this.y0 = Math.min(y0, y1);
     this.x1 = Math.max(x0, x1);
     this.y1 = Math.max(y0, y1);
-    this.offsets = {};
-    this.colors = {};
+
+    this.width = this.x1 - this.x0;
+    this.height = this.y1 - this.y0;
+
+    this.gradientWidth = 2 * (this.width + this.height);
+    this.gradientHeight = 1;
+
+    let offscreenCanvas = this.getOffscreenCanvas(
+      this.gradientWidth,
+      this.gradientHeight
+    );
+    this.offscreenCtx = offscreenCanvas.getContext("2d");
+    this.offscreenGradient = this.offscreenCtx.createLinearGradient(
+      0,
+      0,
+      this.gradientWidth,
+      this.gradientHeight
+    );
   }
 
   addColorStop(offset, color) {
-    const key = offset;
-    this.colors[key] = color;
-    this.offsets[key] = offset;
+    this.offscreenGradient.addColorStop(offset, color);
   }
 
-  getStops() {
-    const stops = Object.values(this.offsets)
-      .sort()
-      .map(stop => {
-        return { stop: stop, color: tinycolor(this.colors[stop]) };
-      });
-    if (stops[0].stop > 0) {
-      stops.unshift({ stop: 0, color: stops[0].color });
+  getOffscreenCanvas(w, h) {
+    if (window.OffscreenCanvas) {
+      return new window.OffscreenCanvas(w, h);
     }
-    if (stops[stops.length - 1].stop !== 1) {
-      stops.push({ stop: 1, color: stops[stops.length - 1].color });
-    }
-    return stops;
+    const offscreenCanvas = document.createElement("canvas");
+    offscreenCanvas.width = w;
+    offscreenCanvas.height = h;
+    return offscreenCanvas;
   }
 
-  colourForProportion(stops, proportion) {
-    let step = 0;
-    for (let i = 0; i < stops.length - 1; i++) {
-      let stopA = stops[i];
-      let stopB = stops[i + 1];
-      if (proportion >= stopA.stop) {
-        if (proportion <= stopB.stop) {
-          step = i;
-          break;
-        }
-      }
-    }
+  getOffscreenGradientData() {
+    const w = this.gradientWidth;
+    const h = this.gradientHeight;
+    this.offscreenCtx.fillStyle = this.offscreenGradient;
+    this.offscreenCtx.fillRect(0, 0, w, h);
+    const imageData = this.offscreenCtx.getImageData(0, 0, w, h);
+    return imageData.data;
+  }
 
-    const regionStart = stops[step];
-    const regionEnd = stops[step + 1];
-
-    const propStart = proportion - regionEnd.stop;
-    const propEnd = proportion - regionStart.stop;
-    const propRange = propEnd - propStart;
-
-    const blend = propEnd / propRange;
-
-    return tinycolor
-      .mix(regionStart.color, regionEnd.color, blend * 100)
-      .toRgbString();
+  colourForProportion(gradientData, proportion) {
+    const x = Math.floor((gradientData.length / 4) * proportion) * 4;
+    return gradientData.slice(x, x + 4);
   }
 
   draw() {
+    const gradientData = this.getOffscreenGradientData();
     const ctx = this.ctx;
-    const width = this.x1 - this.x0;
-    const height = this.y1 - this.y0;
-    const centreX = this.x0 + width / 2;
-    const centreY = this.y0 + height / 2;
+    const centreX = this.x0 + this.width / 2;
+    const centreY = this.y0 + this.height / 2;
     const rectSize = 1;
-    const stops = this.getStops();
     for (let y = this.y0; y < this.y1; y += rectSize) {
       let dY = y - centreY;
       for (let x = this.x0; x < this.x1; x += rectSize) {
@@ -83,7 +75,9 @@ export class SweepGradient {
             angle = angle + 180;
           }
         }
-        ctx.fillStyle = this.colourForProportion(stops, angle / 360);
+        let rgba = this.colourForProportion(gradientData, angle / 360);
+        let [r, g, b, a] = rgba;
+        ctx.fillStyle = `rgba(${r},${g},${b},${a / 255})`;
         ctx.fillRect(x, y, rectSize, rectSize);
       }
     }
